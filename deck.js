8eb54a97b29f4498ca3615b05bccb8d3817fa1d0
Rymedy <rfwebb10@gmail.com>
2021-06-18 23:31:51 +1000

    committing other required files including deck.js, index.html and styles.css

---
+++ deck.js
@@ -0,0 +1,36 @@
+export default class Deck {
+	constructor(cards = freshDeck()) {
+		this.cards = cards
+	}
+
+
+	get numberOfCards() {
+		return this.cards.length
+	}
+
+	shuffle() {
+		for (let i = this.numberOfCards - 1; i > 0; i--) {
+			const newIndex = Math.floor(Math.random() * (i + 1))
+			const oldValue = this.cards[newIndex]
+			this.cards[newIndex] = this.cards[i]
+			this.cards[i] = oldValue
+		}
+	}
+}
+class MinionCard {
+	constructor(attack, health) {
+		this.attack = attack
+		this.health = health
+	}
+}
+
+function freshDeck() {
+	let kobalt_ghost = new MinionCard(1, 2)
+	let meltracer = new MinionCard(3, 1)
+	let spirit_walker = new MinionCard(3, 2)
+	let tarantula = new MinionCard(4, 2)
+	let king_monkey = new MinionCard(4, 5)
+	let captain_merciful = new MinionCard(7, 6)
+
+	return [kobalt_ghost, meltracer, spirit_walker, tarantula, king_monkey, captain_merciful]
+}
