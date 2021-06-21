6fb111e934e769a790d74ba37ca056b3444eb959
Rymedy <49023843+Rymedy@users.noreply.github.com>
2021-06-21 18:25:00 +1000

    Add files via upload

--- deck.js
+++ deck.js
@@ -72,8 +72,8 @@ function freshDeck() {
 	let elegant_mage = new MinionCard(8, 6)
 	let giant_gorilla = new MinionCard(10, 10)
 	let treasure_hoarder = new MinionCard(2, 8)
-	let boosting_totem = new MinionCard(0, 4)
-	let oom_totem = new MinionCard(0, 6)
+	let boosting_totem = new MinionCard(2, 4)
+	let oom_totem = new MinionCard(2, 6)
 	let the_devil = new MinionCard(20, 20)
 	let celient_goomba = new MinionCard(3, 5)
 	let giant_dark_elf= new MinionCard(9, 4)
@@ -96,60 +96,4 @@ function freshDeck() {
 	giant_dark_elf,
 	the_devil
 	]
-export default class Deck {
-	constructor(cards = freshDeck()) {
-		this.cards = cards
-	}
-
-
-	get numberOfCards() {
-		return this.cards.length
-	}
-
-	shuffle() {
-		for (let i = this.numberOfCards - 1; i > 0; i--) {
-			const newIndex = Math.floor(Math.random() * (i + 1))
-			const oldValue = this.cards[newIndex]
-			this.cards[newIndex] = this.cards[i]
-			this.cards[i] = oldValue
-		}
-	}
-}
-class MinionCard {
-	constructor(attack, health) {
-		this.attack = attack
-		this.health = health
-	}
-
-	getHTML() {
-		const cardDiv = document.createElement('div')
-		const cardFaceDiv = document.createElement('div')
-		const cardLabelDiv = document.createElement('div')
-		const attackLabelDiv = document.createElement('div')
-		const healthLabelDiv = document.createElement('div')
-		document.getElementById('cards').appendChild(cardDiv);
-		cardDiv.classList.add("card")
-		cardFaceDiv.classList.add("card-face")
-		cardLabelDiv.classList.add("card-label")
-		cardDiv.appendChild(cardFaceDiv)
-		cardFaceDiv.appendChild(cardLabelDiv)
-		cardFaceDiv.appendChild(attackLabelDiv)
-		cardFaceDiv.appendChild(healthLabelDiv)
-		cardLabelDiv.innerHTML = "1";
-		attackLabelDiv.innerText = this.attack
-		healthLabelDiv.innerText = this.health
-		return cardDiv
-	}
-}
-
-function freshDeck() {
-	let kobalt_ghost = new MinionCard(1, 2)
-	let meltracer = new MinionCard(3, 1)
-	let spirit_walker = new MinionCard(3, 2)
-	let tarantula = new MinionCard(4, 2)
-	let king_monkey = new MinionCard(4, 5)
-	let captain_merciful = new MinionCard(7, 6)
-
-	return [kobalt_ghost, meltracer, spirit_walker, tarantula, king_monkey, captain_merciful]
->>>>>>> 54e0707f515b8b81cf9a7125a59ebd6eabdf98f1
 }
--- index.js
+++ index.js
@@ -105,6 +105,18 @@ $(".cardinplay").on("mousedown", function(e){
       		duration: 200,
       		easing: [.7,.3,.73,.37]
     	}).velocity("reverse", { duration: 600, easing: [.01,.92,.09,.99]});
+    	if(currentAttackerHealth <= 0) {
+    		currentAttacker.hide();
+    	}
+    	if(targetHealth <= 0) {
+    		target.hide();
+    	}
+    	if($("#opposinghero").is(":hidden")) {
+    		alert("You've Won!")
+    	}
+    	if($("#playerhero").is(":hidden")) {
+    		alert("You've Lost!")
+    	}
     	currentAttacker = null;
     	canAttack = false;
   	} else {
