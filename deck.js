4392167fb0a2342cd49dbc1bd3917d0188456166
Rymedy <rfwebb10@gmail.com>
2021-06-21 10:13:46 +1000

    new updates

---
+++ deck.js
@@ -0,0 +1,99 @@
+export default class Deck {
+	constructor(cards = freshDeck()) {
+		this.cards = cards
+	}
+
+
+	get numberOfCards() {
+		return this.cards.length
+	}
+
+	shuffle() {
+		for (let i = this.numberOfCards - 1; i > 0; i--) {
+			const newIndex = Math.floor(Math.random() * (i + 1))
+			const oldValue = this.cards[newIndex]
+			this.cards[newIndex] = this.cards[i]
+			this.cards[i] = oldValue
+		}
+	}
+}
+class MinionCard {
+	constructor(attack, health) {
+		this.attack = attack
+		this.health = health
+		this.mana = Math.round((this.attack + this.health) * 0.5)
+	}
+
+	getPlayerHTML() {
+		const playerCardDiv = document.createElement('div')
+		const playerAttackValue = document.createElement('div')
+		const playerHealthValue = document.createElement('div')
+		playerCardDiv.classList.add("cardinplay")
+		playerCardDiv.classList.add("player-cardinplay")
+		playerAttackValue.classList.add("attackValue")
+		playerHealthValue.classList.add("healthValue")
+		playerCardDiv.appendChild(playerAttackValue)
+		playerCardDiv.appendChild(playerHealthValue)
+		playerAttackValue.innerText = this.attack
+		playerHealthValue.innerText = this.health
+		return playerCardDiv
+	}
+	getComputerHTML() {
+		const computerCardDiv = document.createElement('div')
+		const computerAttackValue = document.createElement('div')
+		const computerHealthValue = document.createElement('div')
+		computerCardDiv.classList.add("cardinplay")
+		computerCardDiv.classList.add("computer-cardinplay")
+		computerAttackValue.classList.add("attackValue")
+		computerHealthValue.classList.add("healthValue")
+		computerCardDiv.appendChild(computerAttackValue)
+		computerCardDiv.appendChild(computerHealthValue)
+		computerAttackValue.innerText = this.attack
+		computerHealthValue.innerText = this.health
+		return computerCardDiv
+	}
+	updatePlayerHTML() {
+		
+	}
+	updateComputerHTML() {
+		
+	}
+}
+
+function freshDeck() {
+	let kobalt_ghost = new MinionCard(1, 2)
+	let meltracer = new MinionCard(3, 1)
+	let spirit_walker = new MinionCard(3, 2)
+	let tarantula = new MinionCard(4, 2)
+	let king_monkey = new MinionCard(4, 5)
+	let captain_merciful = new MinionCard(7, 6)
+	let treasure_pirate = new MinionCard(4, 1)
+	let boombreaker = new MinionCard(1, 4)
+	let elegant_mage = new MinionCard(8, 6)
+	let giant_gorilla = new MinionCard(10, 10)
+	let treasure_hoarder = new MinionCard(2, 8)
+	let boosting_totem = new MinionCard(0, 4)
+	let oom_totem = new MinionCard(0, 6)
+	let the_devil = new MinionCard(20, 20)
+	let celient_goomba = new MinionCard(3, 5)
+	let giant_dark_elf= new MinionCard(9, 4)
+
+	return [
+	kobalt_ghost, 
+	meltracer, 
+	spirit_walker, 
+	tarantula, 
+	king_monkey, 
+	captain_merciful, 
+	treasure_pirate, 
+	boombreaker, 
+	elegant_mage, 
+	giant_gorilla, 
+	treasure_hoarder,
+	boosting_totem,
+	oom_totem,
+	celient_goomba,
+	giant_dark_elf,
+	the_devil
+	]
+}
---
+++ index.js
@@ -0,0 +1,114 @@
+import Deck from "./deck.js"
+var currentAttacker = null;
+var canAttack = new Boolean(null);
+const computerCardSlot = document.querySelector('.board--opponent')
+const playerCardSlot = document.querySelector('.board--player')
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+
+let playerDeck, computerDeck, inRound
+
+startGame()
+function startGame() {
+	const deck = new Deck()
+	deck.shuffle()
+
+	const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+	playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+	computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+	inRound = false
+
+	console.log(playerDeck)
+	console.log(computerDeck)
+
+	updateDeckCount()
+	beginRound()
+	let x = 7
+	for(let i = 0; i < x; i++) {
+		playerCardSlot.appendChild(playerDeck.cards[0].getPlayerHTML())
+		computerCardSlot.appendChild(computerDeck.cards[0].getComputerHTML())
+		playerDeck.cards.shift();
+		computerDeck.cards.shift();
+		updateDeckCount()
+	}
+}
+
+function updateDeckCount() {
+	computerDeckElement.innerText = computerDeck.numberOfCards
+	playerDeckElement.innerText = playerDeck.numberOfCards
+	if (computerDeckElement.innerText === '0') {
+		$("#computerDeck").hide();
+	}
+	else {
+		$("#computerDeck").show();
+	}
+
+	if (playerDeckElement.innerText === '0') {
+		$("#playerDeck").hide();
+	}
+	else {
+		$("#playerDeck").show();
+	}
+}
+
+function beginRound() {
+	canAttack = true;
+}
+
+function updateHealth() {
+
+}
+
+// Attack Animation
+$(".cardinplay").on("mousedown", function(e){
+	if(currentAttacker === null) {
+		if($(this).hasClass("player-cardinplay")) {
+	    	currentAttacker = $(this);
+	    	var xOrigin = e.pageX;
+	        var yOrigin = e.pageY;
+	    	$("svg").show();
+    	}
+	$("body").on("mousemove", function(e2){
+		var xDest = e2.pageX;
+		var yDest = e2.pageY;
+		$("svg path").attr('d', 'M'+xDest+','+yDest+' '+xOrigin+','+yOrigin+'');
+    });
+}
+	else if(($(this).hasClass("computer-cardinplay")) || ($(this).is("#opposinghero"))){
+		var snd = new Audio("attack.mp3");
+    	var target = $(this),
+        xEnd = e.pageX - (target.width()/2),
+        yEnd = e.pageY - (target.height()/2),
+        xStart = currentAttacker.offset().left,
+        yStart = currentAttacker.offset().top;
+        // Attack Algorithm
+        console.log('currentAttacker', currentAttacker.children()[0].innerHTML);
+        var currentAttackerAttack = currentAttacker.children()[0].innerHTML;
+		var currentAttackerHealth = currentAttacker.children()[1].innerHTML;
+		var targetAttack = target.children()[0].innerHTML;
+		var targetHealth = target.children()[1].innerHTML;
+		console.log(currentAttackerAttack, currentAttackerHealth, targetAttack, targetHealth)
+		currentAttackerHealth = currentAttackerHealth - targetAttack
+		targetHealth = targetHealth - currentAttackerAttack
+		currentAttacker.children()[1].innerHTML = currentAttackerHealth
+		target.children()[1].innerHTML = targetHealth
+    
+    	$("svg").hide();
+    	snd.play();
+    
+    	$(currentAttacker).velocity({
+			translateZ: 0,
+			translateX: ""+xEnd - xStart+"px",
+			translateY: ""+(yEnd - yStart)+"px"
+    	},
+    	{
+      		duration: 200,
+      		easing: [.7,.3,.73,.37]
+    	}).velocity("reverse", { duration: 600, easing: [.01,.92,.09,.99]});
+    	currentAttacker = null;
+    	canAttack = false;
+  	} else {
+  		alert("That is not a valid target!");
+  	}
+});
+
