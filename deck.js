3a0ec160935fd6ac406e93dd04e591799a33df4a
Edward Webb <edward.webb9@outlook.com>
2021-06-21 10:21:04 +1000

    last merge conflict error

--- deck.js
+++ deck.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default class Deck {
 	constructor(cards = freshDeck()) {
 		this.cards = cards
@@ -97,61 +96,4 @@ function freshDeck() {
 	giant_dark_elf,
 	the_devil
 	]
-=======
-export default class Deck {
-	constructor(cards = freshDeck()) {
-		this.cards = cards
-	}
-
-
-	get numberOfCards() {
-		return this.cards.length
-	}
-
-	shuffle() {
-		for (let i = this.numberOfCards - 1; i > 0; i--) {
-			const newIndex = Math.floor(Math.random() * (i + 1))
-			const oldValue = this.cards[newIndex]
-			this.cards[newIndex] = this.cards[i]
-			this.cards[i] = oldValue
-		}
-	}
-}
-class MinionCard {
-	constructor(attack, health) {
-		this.attack = attack
-		this.health = health
-	}
-
-	getHTML() {
-		const cardDiv = document.createElement('div')
-		const cardFaceDiv = document.createElement('div')
-		const cardLabelDiv = document.createElement('div')
-		const attackLabelDiv = document.createElement('div')
-		const healthLabelDiv = document.createElement('div')
-		document.getElementById('cards').appendChild(cardDiv);
-		cardDiv.classList.add("card")
-		cardFaceDiv.classList.add("card-face")
-		cardLabelDiv.classList.add("card-label")
-		cardDiv.appendChild(cardFaceDiv)
-		cardFaceDiv.appendChild(cardLabelDiv)
-		cardFaceDiv.appendChild(attackLabelDiv)
-		cardFaceDiv.appendChild(healthLabelDiv)
-		cardLabelDiv.innerHTML = "1";
-		attackLabelDiv.innerText = this.attack
-		healthLabelDiv.innerText = this.health
-		return cardDiv
-	}
-}
-
-function freshDeck() {
-	let kobalt_ghost = new MinionCard(1, 2)
-	let meltracer = new MinionCard(3, 1)
-	let spirit_walker = new MinionCard(3, 2)
-	let tarantula = new MinionCard(4, 2)
-	let king_monkey = new MinionCard(4, 5)
-	let captain_merciful = new MinionCard(7, 6)
-
-	return [kobalt_ghost, meltracer, spirit_walker, tarantula, king_monkey, captain_merciful]
->>>>>>> 54e0707f515b8b81cf9a7125a59ebd6eabdf98f1
 }
