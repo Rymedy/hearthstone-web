c3e6fc9a274c63bb529649d7221247d016d08c43
Richard Webb <rfwebb10@gmail.com>
2021-07-18 23:19:20 +1000

    fix merge conflicts

--- src/scripts/fps.js
+++ src/scripts/fps.js
@@ -1,28 +1,29 @@
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.ieRequestAnimationFrame     ||
-            function( callback ){
-              window.setTimeout(callback, 1000 / 60);
-            };
-  })();
-  
-  var fpsElement = document.getElementById("fps");
-  
-  var then = Date.now() / 1000;  // get time in seconds
-  var render = function() {
-      var now = Date.now() / 1000;  // get time in seconds
-      
-      // compute time since last frame
-      var elapsedTime = now - then;
-      then = now;
-      
-      // compute fps
-      var fps = 1 / elapsedTime;
-      fpsElement.innerText = fps.toFixed(0) + "fps";  
-      requestAnimFrame(render);
-  };
-render();
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.ieRequestAnimationFrame     ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
+var fpsElement = document.getElementById("fps");
+
+// declare an array that stores
+let fps = [];
+
+var render = function() {
+    const now = Date.now();
+    /* delete all frames older than 1000ms */
+    while (fps.length > 0 && fps[0] <= now - 1000) {
+      fps.shift();
+    }
+    /* add a single frame to the fps counter */
+    fps.push(now);
+
+    fpsElement.innerText = fps.length + "fps";  
+    requestAnimFrame(render);
+};
+render();
